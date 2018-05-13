6a45c5f50546fef4a4d8011a80c4ff567bb7e859
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-05-13 21:18:35 +0000

    first

---
+++ functionprogramming.js
@@ -0,0 +1,17 @@
+//Declare Objects as variables
+
+
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+  "wheels":4,
+  "engines":5,
+  "seats":2,
+
+  // Only change code below this line.
+
+};
